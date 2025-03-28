3e4b300351aaeff587833868e2f051d7dd51afcd
sara <saraala7@gmail.com>
2025-03-28 17:16:58 +0100

    Acabat

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -26,24 +26,30 @@ class Program
         Regex rg = new Regex(@"participant ([A-Z]+\w+ [A-Z-'a-z]+\w+).*representa(nt)? (a |de )([A-Z-a-z]+\w+)");
         Regex desquali = new Regex(@"participant ([A-Z]+\w+ [A-Z-'-a-z]+\w+).*(desqualifica(da|t))");
         var jugadors = new Dictionary<string, string>();
-        Dictionary<string, int> punts = new Dictionary<string, int> {{ "X", 0 }, { "O", 0 }};
+        var punts = new Dictionary<string, int> ();
+        var desqualificats = new List<string>();
         
+        //Llista desqualificats
+        foreach (var respostes in resposta)
+        {
+            Match match = desquali.Match(respostes);
+            string nom = match.Groups[1].Value;
+            desqualificats.Add(nom);
+        }
+        //Llista jugadors
         foreach (var respostes in resposta)
         {
             Match match = rg.Match(respostes);
             string nom = match.Groups[1].Value;
             string pais = match.Groups[4].Value;
-            jugadors.Add(nom, pais);
-            Console.WriteLine($"Els jugadors és: {nom}\n Pais:{pais} ");
+            if (!desqualificats.Contains(nom))
+            {
+                jugadors.Add(nom, pais);
+                punts.Add(nom, 0);
+                //Console.WriteLine($"Els jugadors és: {nom}\n Pais:{pais} ");
+            }
         }
-
-        foreach (var respostes in resposta)
-        {
-            Match match = desquali.Match(respostes);
-            string nom = match.Groups[1].Value;
-            jugadors.Remove(nom);
-        }
-
+        
         /*foreach (var jugador in jugadors)
         {
             Console.WriteLine($"El nom: {jugador.Key}, el pais: {jugador.Value}");
@@ -52,7 +58,7 @@ class Program
         //for per revisar totes les 10000 partides de la competició amb el GET a dintre
         for (var i = 1; i <= 10000; i = i++)
         {
-            var partida = await client.GetFromJsonAsync<Resposta>($"partida/{i}"); 
+            var partida = await client.GetFromJsonAsync<Resposta>($"partida/{i}");
             //Console.WriteLine(partida.ToString());
             
             //switch amb 8 ifs per comprovar resultats de cada partida
@@ -60,37 +66,68 @@ class Program
             string dos = partida.tauler[1];
             string tres = partida.tauler[2];
             
-            //bool  espaisbuits = !un.Contains('.') && !dos.Contains('.') && !tres.Contains('.');
-
-            Console.WriteLine($"{partida}");
-            if (un[0] == 'X' && un[1] == 'X' && un[2] == 'X' || dos[0] == 'X' && dos[1] == 'X' && dos[2] == 'X' || tres[0] == 'X' && tres[1] == 'X' && tres[2] == 'X' ||
-                un[0] == 'X' && dos[0] == 'X' && tres[0] == 'X' || un[1] == 'X' && dos[1] == 'X' && tres[1] == 'X' || un[2] == 'X' && dos[2] == 'X' && tres[2] == 'X')
+            var O = "";
+            var X = "";
+            if (punts.ContainsKey(partida.jugador1) && punts.ContainsKey(partida.jugador2))
             {
-                punts["X"]++;
+                O = partida.jugador1;
+                X = partida.jugador2;
+                //Console.WriteLine($"{partida}");
+                if (un[0] == 'X' && un[1] == 'X' && un[2] == 'X' || dos[0] == 'X' && dos[1] == 'X' && dos[2] == 'X' ||
+                    tres[0] == 'X' && tres[1] == 'X' && tres[2] == 'X' ||
+                    un[0] == 'X' && dos[0] == 'X' && tres[0] == 'X' ||
+                    un[1] == 'X' && dos[1] == 'X' && tres[1] == 'X' || un[2] == 'X' && dos[2] == 'X' && tres[2] == 'X')
+                {
+                    punts[$"{X}"]++;
+                }
+                else if (un[0] == 'O' && un[1] == 'O' && un[2] == 'O' ||
+                         dos[0] == 'O' && dos[1] == 'O' && dos[2] == 'O' ||
+                         tres[0] == 'O' && tres[1] == 'O' && tres[2] == 'O' ||
+                         un[0] == 'O' && dos[0] == 'O' && tres[0] == 'O' ||
+                         un[1] == 'O' && dos[1] == 'O' && tres[1] == 'O' ||
+                         un[2] == 'O' && dos[2] == 'O' && tres[2] == 'O')
+                {
+                    punts[$"{O}"]++;
+                }
+                //Diagonals
+                else if (un[0] == 'X' && dos[1] == 'X' && tres[2] == 'X' ||
+                         un[2] == 'X' && dos[1] == 'X' && tres[0] == 'X')
+                {
+                    punts[$"{X}"]++;
+                }
+                else if (un[0] == 'O' && dos[1] == 'O' && tres[2] == 'O' ||
+                         un[2] == 'O' && dos[1] == 'O' && tres[0] == 'O')
+                {
+                    punts[$"{O}"]++;
+                }
+                else
+                {
+                    //Console.WriteLine("Empat");
+                }
             }
-            else if (un[0] == 'O' && un[1] == 'O' && un[2] == 'O' || dos[0] == 'O' && dos[1] == 'O' && dos[2] == 'O' || tres[0] == 'O' && tres[1] == 'O' && tres[2] == 'O' ||
-                     un[0] == 'O' && dos[0] == 'O' && tres[0] == 'O' || un[1] == 'O' && dos[1] == 'O' && tres[1] == 'O' || un[2] == 'O' && dos[2] == 'O' && tres[2] == 'O')
-            {
-                punts["O"]++;
-            }
-            else if (un[0] == 'X' && dos[1] == 'X' && tres[2] == 'X' || un[2] == 'X' && dos[1] == 'X' && tres[0] == 'X')
-            {
-                punts["X"]++;
-            }
-            else if (un[0] == 'O' && dos[1] == 'O' && tres[2] == 'O' || un[2] == 'O' && dos[1] == 'O' && tres[0] == 'O')
-            {
-                punts["O"]++;
-            }
-            else Console.WriteLine("Empat");
             i++;
         }
-        Console.WriteLine($"X: {punts["X"]} \nO: {punts["O"]}");
-        var guanyador = "";
-        if (punts["X"] > punts["O"])
+        foreach (var punt in punts)
         {
-            guanyador = "X";
+            Console.WriteLine($"Nom: {punt.Key}, punts: {punt.Value}");
         }
-        else guanyador = "O";
-        Console.WriteLine($"El guanyador és: {guanyador}");
+        var guanyador = "";
+        var mesgran = 0;
+        foreach (var punt in punts)
+        {
+            if (punt.Value > mesgran)
+            {
+                guanyador = punt.Key;
+                mesgran = punt.Value;
+            }
+        }
+
+        foreach (var jugador in jugadors)
+        {
+            if (jugador.Key == guanyador)
+            {
+                Console.WriteLine($"El guanyador és: {jugador.Key} del país: {jugador.Value}");
+            }
+        }
     }
 }
